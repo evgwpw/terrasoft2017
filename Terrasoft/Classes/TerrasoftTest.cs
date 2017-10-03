7137462e3c3c576b0fda225198c8c61909a7238c
Evgeny <evgeny@vista.aero>
2017-10-03 19:04:54 +0300

    hf

---
+++ Terrasoft/Classes/TerrasoftTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terrasoft.Classes
+{
+    public class TerrasoftTest
+    {
+        /// <summary>
+        /// с использованием LINQ, так бы и сделал скорее всего в продакшене
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static IEnumerable<char> First(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return Enumerable.Empty<char>();
+        }
+    }
+}
